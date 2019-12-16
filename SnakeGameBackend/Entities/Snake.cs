d54af665ec8bd8e70ac6b2fbc31173f688ccd607
Victor <victor@ananias.dev>
2019-12-16 13:38:09 -0300

    screen limit added to snakes

--- SnakeGameBackend/Entities/Snake.cs
+++ SnakeGameBackend/Entities/Snake.cs
@@ -9,7 +9,7 @@ namespace SnakeGameBackend.Entities
         {
             Id = id;
             Head = new SnakePiece(x, y);
-            Pieces = new List<SnakePiece>();
+            Body = new List<SnakePiece>();
             LastUpdate = DateTime.Now;
             ShouldGrow = false;
             Direction = "";
@@ -17,7 +17,7 @@ namespace SnakeGameBackend.Entities
 
         public string Id { get; set; }
         public SnakePiece Head { get; set; }
-        public List<SnakePiece> Pieces { get; set; }
+        public List<SnakePiece> Body { get; set; }
         public bool ShouldGrow { get; set; }
         public DateTime LastUpdate { get; set; }
         public string Direction { get; set; }
@@ -28,17 +28,34 @@ namespace SnakeGameBackend.Entities
             {
                 case "up":
                     Head.Y -= Head.Size;
+                    if (Head.Y < 0)
+                    {
+                        Head.Y = 500;
+                    }
+
                     break;
                 case "right":
                     Head.X += Head.Size;
+                    if (Head.X > 500)
+                    {
+                        Head.X = 0;
+                    }
                     break;
 
                 case "down":
                     Head.Y += Head.Size;
+                    if (Head.Y > 500)
+                    {
+                        Head.Y = 0;
+                    }
                     break;
 
                 case "left":
                     Head.X -= Head.Size;
+                    if (Head.X < 0)
+                    {
+                        Head.X = 500;
+                    }
                     break;
             }
         }
