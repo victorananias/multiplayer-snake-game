488dd752ee53937977f1b0150c7862dcd9867af3
Victor <victor@ananias.dev>
2021-01-11 20:50:56 -0300

    frontend refactored

--- MultiplayerSnakeGame/wwwroot/js/Index.js
+++ MultiplayerSnakeGame/wwwroot/js/Index.js
@@ -2,6 +2,7 @@
 
 import Connection from './Connection.js'
 import Control from './Control.js'
+import KeyboardEvents from './KeyboardEvents.js'
 import View from './View.js'
 
 Connection.establishConnectionAndJoinGame()
